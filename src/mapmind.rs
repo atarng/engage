721b15d452f11559bfda028728731e2056971c7d
The Doge <alex.ren@gmail.com>
2023-09-14 23:25:49 -0700

    comments

--- src/mapmind.rs
+++ src/mapmind.rs
@@ -1,3 +1,4 @@
+//! Types and methods to query the state of [`Unit`](crate::gamedata::unit::Unit)s on the zoomed-out map view.
 use unity::prelude::*;
 
 use crate::{gamedata::unit::Unit, util::get_instance};
@@ -8,13 +9,17 @@ pub struct MapMind {}
 
 // App.MapMind$$get_Unit	7101dee2b0	App_Unit_o * App.MapMind$$get_Unit(App_MapMind_o * __this, MethodInfo * method)	12
 #[unity::from_offset("App", "MapMind", "get_Unit")]
-fn get_unit(this: &Il2CppObject<MapMind>, method_info: OptionalMethod) -> &'static Il2CppObject<Unit>;
+fn get_unit(
+    this: &Il2CppObject<MapMind>,
+    method_info: OptionalMethod,
+) -> &'static Il2CppObject<Unit>;
 
 impl MapMind {
     pub fn get_instance() -> &'static mut Il2CppObject<MapMind> {
         get_instance::<MapMind>()
     }
 
+    /// Seems to get the current unit that is selected by the player. Needs more experimentation.
     pub fn get_unit() -> &'static Il2CppObject<Unit> {
         let instance = Self::get_instance();
         unsafe { get_unit(instance, None) }
