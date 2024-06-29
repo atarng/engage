51db3a916a642331c4a933fa8375404f4c703db6
Alex Ren <alex.ren@gmail.com>
2024-06-28 23:26:31 -0700

    gmap

--- src/sequence.rs
+++ src/sequence.rs
@@ -1,6 +1,7 @@
 //! Modules to manipulate and interact with the various Sequences of the game. Tied to the [`ProcInst`](crate::proc::ProcInst) system.
 
 pub mod configsequence;
+pub mod gmap_sequence;
 pub mod hubrefineshopsequence;
 pub mod mainmenusequence;
 pub mod mainsequence;
