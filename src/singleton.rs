80dc82783a2e2d1122d35090267ef48351ca0c4e
Raytwo <Raytwo@users.noreply.github.com>
2024-10-18 20:08:40 +0200

    Update singleton.rs

--- src/singleton.rs
+++ src/singleton.rs
@@ -7,3 +7,7 @@ pub struct SingletonProcInst { }
 #[repr(C)]
 #[unity::class("App", "SingletonClass`1")]
 pub struct SingletonClass { }
+
+#[repr(C)]
+#[unity::class("App", "SingletonMonoBehaviour`1")]
+pub struct SingletonMonoBehaviour { }
