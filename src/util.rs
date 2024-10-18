10637530bceccf1d8aa285dd109dca52b14339cb
Raytwo <Raytwo@users.noreply.github.com>
2024-10-18 20:10:43 +0200

    Update util.rs

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
-use crate::singleton::SingletonClass;
+use crate::singleton::{SingletonClass, SingletonMonoBehaviour};
 use unity::{il2cpp::class::Il2CppRGCTXData, prelude::*};
 
 /// Utility function to get the instance of a singleton class.
@@ -10,6 +10,20 @@ pub fn get_instance<T: unity::prelude::Il2CppClassData>() -> &'static mut T {
     let get_instance = unsafe {
         std::mem::transmute::<_, extern "C" fn(OptionalMethod) -> &'static mut T>(
             pointer[5].method_ptr,
+        )
+    };
+
+    get_instance(Some(&pointer[5]))
+}
+
+pub fn get_instance_monobehaviour<T: unity::prelude::Il2CppClassData>() -> &'static mut T {
+    let idk = get_generic_class!(SingletonMonoBehaviour<T>).unwrap();
+    let pointer = unsafe {
+        &*(idk.rgctx_data as *const Il2CppRGCTXData as *const u8 as *const [&'static MethodInfo; 5])
+    };
+    let get_instance = unsafe {
+        std::mem::transmute::<_, extern "C" fn(OptionalMethod) -> &'static mut T>(
+            pointer[3].method_ptr,
         )
     };
 
