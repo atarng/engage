50462159c9f5ecb72c61741e4a7e09df04e86064
Raytwo <Raytwo@users.noreply.github.com>
2024-11-28 22:37:04 +0100

    Update godpool.rs

--- src/godpool.rs
+++ src/godpool.rs
@@ -1,10 +1,15 @@
 use unity::prelude::*;
 use crate::gamedata::{GodData, unit::GodUnit};
+
 // App.GodPool$$HasArmlet	71023359c0	bool App.GodPool$$HasArmlet(MethodInfo * method)	464
 #[unity::class("App", "GodPool")]
 pub struct GodPool {}
 
 impl GodPool {
+    pub fn create(god: &GodData) -> Option<&GodUnit> {
+        unsafe { god_pool_create(god, None) }
+    }
+    
     pub fn try_get_gid(gid: &str, include_reserved: bool) -> Option<&GodUnit> {
         unsafe { god_pool_try_get_gid(gid.into(), include_reserved, None) } 
     }
@@ -21,3 +26,6 @@ pub fn god_pool_try_get_gid(gid: &Il2CppString, include_reserved: bool, method_i
 
 #[skyline::from_offset(0x02334600)]
 pub fn god_pool_try_get(god: &GodData, include_reserved: bool, method_info: OptionalMethod) -> Option<&GodUnit>;
+
+#[skyline::from_offset(0x023349c0)]
+pub fn god_pool_create(god: &GodData, method_info: OptionalMethod) -> Option<&GodUnit>;
