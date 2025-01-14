48d243511c00e7450044d79ccc644893c123ede8
Raytwo <Raytwo@users.noreply.github.com>
2025-01-14 17:48:13 +0100

    Update enum.rs

--- src/map/enum.rs
+++ src/map/enum.rs
@@ -31,6 +31,39 @@ impl RangeEnumerator {
   }
 }
 
+impl Iterator for RangeEnumerator {
+    // Maybe a (x, z) tuple with the current tile being checked?
+    type Item = (i32, i32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+      loop {
+        if self.max_x == self.current.x {
+          if self.min_z == self.current.z {
+            // We went through all the possible positions
+            return None;
+          } else {
+            // We haven't gone through every z position yet, restart the loop from the leftmost X position and check again
+            self.current.z -= 1;
+            self.current.x = self.min_x;
+          }
+        } else {
+          // We haven't reached max_x yet, so continue
+          self.current.x += 1;
+        }
+
+        let piv_x = (self.current.x - self.pivot_x).abs();
+        let piv_z = (self.current.z - self.pivot_z).abs();
+
+        let piv = piv_x + piv_z;
+
+        if (piv >= self.near) && (piv <= self.far) {
+          break;
+        }
+      }
+
+      Some((self.current.x, self.current.z))
+    }
+}
 
 #[skyline::from_offset(0x24c54d0)]
 extern "C" fn rangeenumerator_getenumerator(this: &RangeEnumerator, method_info: OptionalMethod) -> RangeEnumerator;
