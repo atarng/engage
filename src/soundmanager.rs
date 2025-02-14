370b1f0cf91380dc9d67cd4d33e913519e2820f0
Raytwo <Raytwo@users.noreply.github.com>
2025-02-14 18:19:55 +0100

    Update soundmanager.rs

--- src/soundmanager.rs
+++ src/soundmanager.rs
@@ -1,8 +1,31 @@
 use unity::prelude::*;
 use crate::combat::Character;
 
+// Careful, this is a nested class, do not instantiate this yourself
+#[unity::class("App.SoundSystem", "SoundHandle")]
+pub struct SoundHandle {}
+
+impl SoundHandle {
+    pub fn get_event_name(&self) -> &'static mut Il2CppString {
+        self.get_class()
+            .get_virtual_method("GetEventName")
+            .map(|method| {
+                let get_event_name = unsafe {
+                    std::mem::transmute::<_, extern "C" fn(&Self, &MethodInfo) -> &'static mut Il2CppString>(method.method_info.method_ptr)
+                };
+
+                get_event_name(&self, method.method_info)
+            })
+            .unwrap()
+    }
+}
+
 #[unity::class("App", "SoundManager")]
-pub struct SoundManager {}
+pub struct SoundManager {
+    padding: [u8; 0x88],
+    sound_handle_list: &'static List<SoundHandle>,
+    // ...
+}
 
 #[unity::class("", "SoundHandle")]
 pub struct SoundSystemSoundHandle {}
@@ -14,6 +37,15 @@ impl SoundManager {
 
     pub fn post_event(&self, event_name: impl AsRef<str>, character: Option<&Character>, is_get_position: bool) -> &'static SoundSystemSoundHandle {
         unsafe { soundmanager_post_event(self, event_name.as_ref().into(), character, is_get_position, None) }
+    }
+
+    pub fn is_event_playing_with_prefix<S: AsRef<str>>(&self, event_name: S) -> bool {
+        let event_name = event_name.as_ref();
+
+        self.sound_handle_list
+            .iter()
+            .find(|handle| handle.get_event_name().to_string().starts_with(event_name))
+            .is_some()
     }
 }
 
